207782d7fac7c273308d549bfd05ea5de52c0e1f
Layanb04 <lalmalki0045@stu.kau.edu.sa>
2024-09-01 01:30:22 +0300

    First Commit

---
+++ src/main/java/group1/layangithub/LayanGitHub.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package group1.layangithub;
+
+/**
+ *
+ * @author almal
+ */
+public class LayanGitHub {
+
+    public static void main(String[] args) {
+
+    // code
+
+    }
+}
